6a6f7bfaa59752826fdb193da7bac394050892d0
Harsis <harshishyadav@gmail.com>
2025-01-12 19:14:15 +0100

    fix img case sensitivity for GH pages

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -5,7 +5,7 @@ export default function Overlay() {
     <div className="absolute top-0 left-0 w-full h-full">
       <Image
         className="absolute bottom-0 left-28 translate-x-[-50%] translate-y-[-50%]"
-        src="Controls.png"
+        src="controls.png"
         alt="logo"
         width="150"
         height="150"
